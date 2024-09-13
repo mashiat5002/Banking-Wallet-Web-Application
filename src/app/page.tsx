32b4600da149f7c06e7692067ab4d0ef711d8d66
Mashiat Islam <mashiath342@gmail.com>
2024-09-13 20:53:08 +0600

    link-token recieved and integrated plaid api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
+import Button from "./PlaidButton/Button";
+
+
 export default async function Home() {
-  const resp = await fetch("http://localhost:3000/api/create-link-token", {
-    method: "POST", // Ensure it's a POST request
-  });
-  
-  const data = await resp.json(); // Parse the JSON response
-  console.log(data);
+
 
   return (
     <div>
-      {/* Render your UI */}
+      <Button />
     </div>
   );
 }
