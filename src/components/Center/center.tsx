eaea45413f777e59952ad0beb14fb7b28df65761
Mashiat Islam <mashiath342@gmail.com>
2024-09-14 16:19:11 +0600

    Landing page center portion completion without functioning

--- src/components/Center/center.tsx
+++ src/components/Center/center.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Mobile_hand from '../Mobile-hand/mobile-hand'
+import Center_left from '../Center_Left/Center_left'
 
 export default function Center() {
   return (
-    <div className='flex flex-row-reverse  mr-52'>
+    <div className='flex flex-row-reverse  mr-52 text-slate-200'>
       <Mobile_hand />
+      <Center_left />
     </div>
   )
 }
--- src/components/Mobile-hand/mobile-hand.tsx
+++ src/components/Mobile-hand/mobile-hand.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 export default function Mobile_hand() {
   return (
-    <div className=''>
+    <div className='w-1/3'>
      <Image height={500} width={500} src='/assets/images/mbl-hand.jpg' alt=''/>
     </div>
   )
