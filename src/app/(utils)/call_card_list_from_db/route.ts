7f5b6d4272ba15bca14c9ceba62aac7669a9d6a7
Mashiat Islam <mashiath342@gmail.com>
2025-02-21 02:12:32 +0600

    more local host issues resolved

--- src/app/(utils)/call_card_list_from_db/route.ts
+++ src/app/(utils)/call_card_list_from_db/route.ts
@@ -1,7 +1,7 @@
 
 
 export async function call_card_list_from_db() {
-    const res= await fetch(`http://localhost:3000/api/card_list_from_db`,{
+    const res= await fetch(`${process.env.NEXT_PUBLIC_Base_Url}/api/card_list_from_db`,{
         method:"POST",
         headers:{
             "Content-Type": "application/json"
