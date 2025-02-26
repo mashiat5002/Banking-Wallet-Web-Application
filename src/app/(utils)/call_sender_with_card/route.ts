c464068edadc8dda1c6aedcada5cb7a5d84967fa
Mashiat Islam <mashiath342@gmail.com>
2025-02-26 15:09:16 +0600

    issue

--- src/app/(utils)/call_sender_with_card/route.ts
+++ src/app/(utils)/call_sender_with_card/route.ts
@@ -42,6 +42,7 @@ export async function call_Sender_card(amount:string,sender:string,receiver:stri
         })
       
         const data= await res.json()
+        console.log(data)
         if(data.response.status=="succeeded")
         {
             await call_spend_card_balance(amount);
