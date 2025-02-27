6115e07bd7b961cd85da441484af5c77b3597847
Mashiat Islam <mashiath342@gmail.com>
2025-02-27 20:55:03 +0600

    issue

--- src/app/api/create-link-token/route.ts
+++ src/app/api/create-link-token/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(createTokenResponse.data); 
   } catch (error) {
     console.error('Error creating link token:', error);
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json({ error: error,plaidRequest }, { status: 500 });
   }
 }
 
